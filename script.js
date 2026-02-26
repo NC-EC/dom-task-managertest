2127c59894ffb35c933c4d3cfd584f054f577e93
NC-EC <101228620+NC-EC@users.noreply.github.com>
2026-02-25 23:50:19 -0600

    Add event listener for button click

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const input = document.getElementById("taskInput");
+
 const button = document.getElementById("addBtn");
+
 const list = document.getElementById("taskList");
 
 button.addEventListener("click", () => {
